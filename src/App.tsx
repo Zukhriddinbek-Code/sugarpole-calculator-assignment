23f0684cd796458a74932e20f7da7151e6530f5b
Zukhriddinbek-Code <zuhriddinganiyev2000@gmail.com>
2024-03-13 20:19:26 +0900

    installing tailwind and init

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,9 @@
-function App() {
-  return <div>Calculator app</div>;
-}
+const App = () => {
+  return (
+    <div>
+      <p className="text-3xl font-bold text-cyan-700">Hello world</p>
+    </div>
+  );
+};
 
 export default App;
