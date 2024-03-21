017906b1b32381d6edffc2c1ffc4e05801d8d2fa
Zukhriddinbek-Code <zuhriddinganiyev2000@gmail.com>
2024-03-21 15:02:46 +0900

    almost done with styling, layout left

--- src/Calculator.tsx
+++ src/Calculator.tsx
@@ -23,49 +23,134 @@ const Calculator = () => {
 
   return (
     //wrapper
-    <div className="flex! flex-col! w-400 h-367 bg-customBack items-center! justify-center! font-body p-25">
+    <div className="w-400 h-367 bg-customBack font-body p-25">
       {/* display */}
       <div className="flex items-center justify-end h-70 w-350 rounded-20 bg-screen text-screenText font-body font-normal text-customSize">
         {input}
       </div>
       {/* buttonbox */}
       <div className="grid grid-cols-5 grid-rows-4 font-body">
-        <button onClick={() => handleButtonClick("1")}>1</button>
-        <button onClick={() => handleButtonClick("2")}>2</button>
-        <button onClick={() => handleButtonClick("3")}>3</button>
-        <button onClick={() => handleButtonClick("*")} className="operator">
-          X
+        <button
+          className="border-none bg-btnBack font-normal font-body text-btnCustom text-screenText cursor-pointer rounded-[35px] w-[65px] h-[45px]"
+          onClick={() => handleButtonClick("1")}
+        >
+          1
         </button>
-        <button onClick={() => handleButtonClick("/")} className="operator">
+        <button
+          className="border-none bg-btnBack font-normal font-body text-btnCustom text-screenText cursor-pointer rounded-[35px] w-[65px] h-[45px]"
+          onClick={() => handleButtonClick("2")}
+        >
+          2
+        </button>
+        <button
+          className="border-none bg-btnBack font-normal font-body text-btnCustom text-screenText cursor-pointer rounded-[35px] w-[65px] h-[45px]"
+          onClick={() => handleButtonClick("3")}
+        >
+          3
+        </button>
+        <button
+          className="bg-btnOperator border-none w-[45px] h-[45px] font-body text-screenText font-normal text-customSize rounded-full"
+          onClick={() => handleButtonClick("*")}
+        >
+          x
+        </button>
+        <button
+          className="bg-btnOperator border-none w-[45px] h-[45px] font-body text-screenText font-normal text-customSize rounded-full"
+          onClick={() => handleButtonClick("/")}
+        >
           ÷
         </button>
 
-        <button onClick={() => handleButtonClick("4")}>4</button>
-        <button onClick={() => handleButtonClick("5")}>5</button>
-        <button onClick={() => handleButtonClick("6")}>6</button>
-        <button onClick={() => handleButtonClick("+")} className="operator">
+        <button
+          className="border-none bg-btnBack font-normal font-body text-btnCustom text-screenText cursor-pointer rounded-[35px] w-[65px] h-[45px]"
+          onClick={() => handleButtonClick("4")}
+        >
+          4
+        </button>
+        <button
+          className="border-none bg-btnBack font-normal font-body text-btnCustom text-screenText cursor-pointer rounded-[35px] w-[65px] h-[45px]"
+          onClick={() => handleButtonClick("5")}
+        >
+          5
+        </button>
+        <button
+          className="border-none bg-btnBack font-normal font-body text-btnCustom text-screenText cursor-pointer rounded-[35px] w-[65px] h-[45px]"
+          onClick={() => handleButtonClick("6")}
+        >
+          6
+        </button>
+        <button
+          className="bg-btnOperator border-none w-[45px] h-[45px] font-body text-screenText font-normal text-customSize rounded-full"
+          onClick={() => handleButtonClick("+")}
+        >
           +
         </button>
-        <button onClick={() => handleButtonClick("-")} className="operator">
+        <button
+          className="bg-btnOperator border-none w-[45px] h-[45px] font-body text-screenText font-normal text-customSize rounded-full"
+          onClick={() => handleButtonClick("-")}
+        >
           -
         </button>
 
-        <button onClick={() => handleButtonClick("7")}>7</button>
-        <button onClick={() => handleButtonClick("8")}>8</button>
-        <button onClick={() => handleButtonClick("9")}>9</button>
-        <button onClick={() => handleButtonClick("%")} className="operator">
+        <button
+          className="border-none bg-btnBack font-normal font-body text-btnCustom text-screenText cursor-pointer rounded-[35px] w-[65px] h-[45px]"
+          onClick={() => handleButtonClick("7")}
+        >
+          7
+        </button>
+        <button
+          className="border-none bg-btnBack font-normal font-body text-btnCustom text-screenText cursor-pointer rounded-[35px] w-[65px] h-[45px]"
+          onClick={() => handleButtonClick("8")}
+        >
+          8
+        </button>
+        <button
+          className="border-none bg-btnBack font-normal font-body text-btnCustom text-screenText cursor-pointer rounded-[35px] w-[65px] h-[45px]"
+          onClick={() => handleButtonClick("9")}
+        >
+          9
+        </button>
+        <button
+          className="bg-btnOperator border-none w-[45px] h-[45px] font-body text-screenText font-normal text-customSize rounded-full"
+          onClick={() => handleButtonClick("%")}
+        >
           %
         </button>
-        <button onClick={() => handleButtonClick("^")} className="operator">
+        <button
+          className="bg-btnOperator border-none w-[45px] h-[45px] font-body text-screenText font-normal text-customSize rounded-full"
+          onClick={() => handleButtonClick("^")}
+        >
           ^
         </button>
 
-        <button onClick={() => handleButtonClick(".")}>.</button>
-        <button onClick={() => handleButtonClick("0")}>0</button>
-        <button onClick={handleClear}>
-          <img src="clear.png" alt="clear" width={33} height={24} />
+        <button
+          className="border-none bg-btnBack font-normal font-body text-btnCustom text-screenText cursor-pointer rounded-[35px] w-[65px] h-[45px]"
+          onClick={() => handleButtonClick(".")}
+        >
+          .
         </button>
-        <button onClick={() => handleCalculate} className="operator">
+        <button
+          className="border-none bg-btnBack font-normal font-body text-btnCustom text-screenText cursor-pointer rounded-[35px] w-[65px] h-[45px]"
+          onClick={() => handleButtonClick("0")}
+        >
+          0
+        </button>
+        <button
+          className="border-none bg-btnBack font-normal font-body text-btnCustom text-screenText cursor-pointer rounded-[35px] w-[65px] h-[45px]"
+          onClick={handleClear}
+        >
+          <img
+            src="clear.png"
+            alt="clear"
+            width={33}
+            height={24}
+            className=""
+          />
+        </button>
+        <button
+          className="w-[104.83px] h-[46.8px] rounded-[100px] bg-btnEqualBg font-body font-normal text-btnEqualText text-btnEqual"
+          onClick={() => handleCalculate}
+        >
           =
         </button>
       </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -8,6 +8,8 @@ export default {
       },
       fontSize: {
         customSize: ["32px", "38.73px"],
+        btnCustom: ["24px", "29.05px"],
+        btnEqual: ["28px", "33.89px"],
       },
       width: { 400: "400px", 350: "350px" },
       height: {
@@ -19,6 +21,10 @@ export default {
         customBack: "#242530",
         screen: "#3A3F77",
         screenText: "#FFFFFF",
+        btnBack: "#404258",
+        btnOperator: "#F49D1A",
+        btnEqualBg: "#B2B2B2",
+        btnEqualText: "#000000",
       },
       borderRadius: {
         20: "20px",
